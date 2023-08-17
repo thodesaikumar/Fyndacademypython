ef727ffbbc9fc03ef87fe9d81473a8b44bd6de58
thodesaikumar <125036699+thodesaikumar@users.noreply.github.com>
2023-08-17 12:43:12 +0530

    mail

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-export DATABASE_USER=vijay
+export DATABASE_USER=saikumar
 export DATABASE_PASSWORD=HelloWorld123#
 export DATABASE_SERVER=localhost
 export DATABASE_NAME=result_server
-export MAIL_USERNAME=vijaybomma0106@gmail.com
+export MAIL_USERNAME=saikumar010666@gmail.com
 export MAIL_PASSWORD=yqgfajbztvwqmnyv 
-export MAIL_FROM=vijaybomma0106@gmail.com
+export MAIL_FROM=saikumar010666@gmail.com
 export MAX_ATTEMPTS=3
 export OTP_EXPIRY_SECONDS=60
 export PORT=8000
